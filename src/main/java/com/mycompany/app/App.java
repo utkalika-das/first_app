ee26dbefee9904dfcc54fb15a6c74d75c71e5525
utkalika <you@example.com>
2025-12-05 11:19:38 +0530

    Initial: print natural numbers 1..10

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class App {
+    public static void main(String[] args) {
+        // Print natural numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
